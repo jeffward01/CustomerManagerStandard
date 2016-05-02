47536d3373ab40926e854b893618878b16cdb937
jeffward01 <jeffwardcoding@gmail.com>
2016-05-02 15:33:55 -0700

    added typescript and webpack

---
+++ CustomerManager/webpack.config.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+    context: __dirname,
+    entry: "./index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js" // no hash in main.js because index.html is a static page
+    }
+};
